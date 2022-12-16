dfaef36c3ff3f7c223aa5afb292e7bc2ae193c4d
“S3lk1rk” <matthew.selkirk51@gmail.com>
2022-12-16 19:56:14 +0000

    Change

--- client/src/components/menuitem.js
+++ client/src/components/menuitem.js
@@ -4,30 +4,31 @@ import FetchNutrition from "./FetchNutrition";
 import Review from "./Review";
 
 const Item = ({ food }) => {
-const [OpenNutrition, SetOpenNutrition] = useState(false);
+  const [OpenNutrition, SetOpenNutrition] = useState(false);
 
 
 
   return (
     <div>
-      
+
       <Accordion.Header> {food.name}</Accordion.Header>
-      
+
       <Accordion.Body>
-      <p>{food.category}</p>
+        <p>{food.category}</p>
         <p>{food.description}</p>
         <p>{food.price}</p>
         <p>{food.allergens}</p>
-         
+
         <button onClick={() => SetOpenNutrition(!OpenNutrition)}>Show Nutrition</button>
         {OpenNutrition && <FetchNutrition query={food.name} />}
-        
-        <p><Review food={food}/></p>
+
+        <p><Review food={food} /></p>
         <p>{food.review}</p>
-        <button onClick={() =>  {food.menu.splice(0, food.menu.length)}}> Remove from Menu </button>
+        <button onClick={() => { food.menu.splice(0, food.menu.length) }}> Remove from Menu </button>
       </Accordion.Body>
-</div>
+    </div>
 
 
-); };
+  );
+};
 export default Item;
