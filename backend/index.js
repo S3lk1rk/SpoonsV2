047cc5b1b4f13de4070a5cc6b3fbc187313ee9df
“S3lk1rk” <matthew.selkirk51@gmail.com>
2022-12-17 00:48:29 +0000

    Sodoku

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(__filename);
 
 const cors = require('cors');
 app.use(cors());
@@ -12,6 +13,13 @@ app.use(express.urlencoded({extended: false }));
 const router = require('./routes/routes');
 app.use('/', router);
 
-app.listen(3001, () => {
+app.use(express.static(path.join(dirname, "client", "build")))
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(dirname, "client", "build", "index.html"));
+})
+
+app.listen(process.env.port || 3001, () => {
   console.log("Server started on port 3001. Ctrl^c to quit.");
 });
